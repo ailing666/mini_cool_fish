01cb9a952b470d8ee552a78f2f4173691585fc1d
xatongxue <1120877019@qq.com>
2020-08-19 14:41:54 +0800

    课程

--- pages/course/course.js
+++ pages/course/course.js
@@ -1,66 +1,59 @@
-// pages/course/course.js
+import request from '../../utils/request'
 Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    courseList: [],
+    levelList: ['初级课程', '中级课程', '高级课程']
+  },
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getCourseList()
+  },
+  async getCourseList () {
+    let res = await request({
+      url: '/api/course/list'
+    })
+    this.setData({
+      courseList: res.message
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {},
 
-    },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {},
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {},
 
-    },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {},
 
-    },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {}
 })
